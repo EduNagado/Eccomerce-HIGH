a04aa27c949e01b224efc93af625d26f93bfd9d6
EduNagado <eduardo.nagado@gmail.com>
2025-01-25 00:12:40 -0300

    fix: Arrumando as categorias por produto

---
+++ src/app/produto/[id]/page.tsx
@@ -0,0 +1,171 @@
+"use client"
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const idproduto = [
+  {
+    id: 1,
+    name: "Tee Paris Black",
+    href: "#",
+    imageSrc: [
+      "/imgPodutos/camisa1.png",
+      "/imgPodutos/camisa1a.png",
+      "/imgPodutos/camisa1b.png",
+      "/imgPodutos/camisa1c.png",
+      "/imgPodutos/medidaCamisa.png",
+    ],
+    description: "Descrição do produto Tee Paris Black.",
+    price: "R$199",
+    sizes: ["XGG", "P", "M", "G", "GG"],
+  },
+  {
+    id: 2,
+    name: "Tee Paris White",
+    href: "#",
+    imageSrc: [
+      "/imgPodutos/camisa2.png",
+      "/imgPodutos/camisa2a.png",
+      "/imgPodutos/camisa2b.png",
+      "/imgPodutos/camisa2c.png",
+      "/imgPodutos/medidaCamisa.png",
+    ],
+    description: "Descrição do produto Tee Paris White.",
+    price: "R$199",
+    sizes: ["XGG", "P", "M", "G", "GG"],
+  },
+  {
+    id: 3,
+    name: "TJ x HIGH Denim Jacket",
+    href: "#",
+    imageSrc: [
+      "/imgPodutos/Blusa1.png",
+      "/imgPodutos/blusa1a.png",
+      "/imgPodutos/blusa1b.png",
+      "/imgPodutos/blusa1c.png",
+      "/imgPodutos/medidaBlusa.png",
+    ],
+    description: "Descrição do produto TJ x HIGH Denim Jacket.",
+    price: "R$699",
+    sizes: ["M", "G", "GG"],
+  },
+];
+
+export default function Produto() {
+  const [isClient, setIsClient] = useState(false);
+  const [quantidade, setQuantidade] = useState(1);
+  const [imagemSelecionada, setImagemSelecionada] = useState("");
+
+  // Garantir que o código do router rode apenas no cliente
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Ou algum loading
+  }
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  // Verifica e converte o ID para um número
+  const numericId = id ? parseInt(Array.isArray(id) ? id[0] : id, 10) : NaN;
+
+  if (isNaN(numericId)) {
+    return <div>ID inválido</div>;
+  }
+
+  const produtoSelecionado = idproduto.find((produto) => produto.id === numericId);
+
+  if (!produtoSelecionado) {
+    return <div>Produto não encontrado</div>;
+  }
+
+  const incrementar = () => {
+    setQuantidade(quantidade + 1);
+  };
+
+  const decrementar = () => {
+    if (quantidade > 1) {
+      setQuantidade(quantidade - 1);
+    }
+  };
+
+  useEffect(() => {
+    // Define a imagem inicial ao carregar o produto
+    if (produtoSelecionado?.imageSrc.length > 0) {
+      setImagemSelecionada(produtoSelecionado.imageSrc[0]);
+    }
+  }, [produtoSelecionado]);
+
+  return (
+    <section className="flex p-6 bg-white">
+      {/* Coluna das imagens */}
+      <div className="w-1/2 flex flex-col items-start">
+        {/* Imagem principal */}
+        <img
+          src={imagemSelecionada}
+          alt="Imagem selecionada"
+          className="w-full h-80 object-contain mb-4"
+        />
+        {/* Imagens menores */}
+        <div className="flex flex-col space-y-2">
+          {produtoSelecionado.imageSrc.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Imagem ${index + 1}`}
+              className="w-16 h-16 cursor-pointer rounded border border-gray-300 object-cover"
+              onClick={() => setImagemSelecionada(image)}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Coluna das informações do produto */}
+      <div className="w-1/2 flex flex-col justify-start items-start p-6">
+        <h1 className="text-3xl font-bold mb-2">{produtoSelecionado.name}</h1>
+        <p className="mb-2">{produtoSelecionado.description}</p>
+        <p className="text-xl font-bold mb-4">{produtoSelecionado.price}</p>
+        <p className="mb-4">
+          Detalhes do produto: <br />
+          • 14oz <br />
+          • Faixa personalizada Tommy <br />
+          • Aviamentos de metal em ouro velho <br />
+          • Bordado peito e costas
+        </p>
+        <div className="mb-4">
+          <h6 className="font-semibold mb-2">TAMANHO</h6>
+          <div className="flex space-x-2">
+            {produtoSelecionado.sizes.map((size) => (
+              <p key={size} className="bg-black text-white py-1 px-3">
+                {size}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        {/* Controle de quantidade */}
+        <div className="flex items-center mb-4 space-x-4">
+          <div className="flex items-center bg-slate-200 w-64">
+            <button
+              onClick={decrementar}
+              className="bg-slate-200 text-black w-1/4 py-3"
+            >
+              -
+            </button>
+            <span className="flex-1 text-center">{quantidade}</span>
+            <button
+              onClick={incrementar}
+              className="bg-slate-200 text-black w-1/4 py-3"
+            >
+              +
+            </button>
+          </div>
+          <button className="bg-black text-white py-3 px-4 w-64 border border-transparent hover:bg-white hover:border-black hover:text-black transition-colors duration-200">
+            Comprar
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,130 +1,162 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const products = [
-    {
-        id: 1,
-        name: 'Tee Paris Black',
-        href: '#',
-        imageSrc: '/imgPodutos/camisa1.png',
-        imageAlt: "Front of men's Tee Paris Black.",
-        price: 'R$199',
-    },
-    {
-        id: 2,
-        name: 'Tee Paris White',
-        href: '#',
-        imageSrc: '/imgPodutos/camisa2.png',
-        imageAlt: "Front of men's Tee Paris White.",
-        price: 'R$199',
-    },
-    {
-        id: 3,
-        name: 'TJ x HIGH Denim Jacket',
-        href: '#',
-        imageSrc: '/imgPodutos/Blusa1.png',
-        imageAlt: "Front of TJ x HIGH Denim Jacket.",
-        price: 'R$699',
-    },
-    {
-        id: 4,
-        name: 'TJ x HIGH Nylon Jacket',
-        href: '#',
-        imageSrc: '/imgPodutos/blusa2.png',
-        imageAlt: "Front of TJ x HIGH Nylon Jacket.",
-        price: 'R$849',
-    },
-    {
-        id: 5,
-        name: 'Hoodie Logo Line Night Black',
-        href: '#',
-        imageSrc: '/imgPodutos/moletom1.png',
-        imageAlt: "Front of Hoodie Logo Line Night Black.",
-        price: 'R$439',
-    },
-    {
-        id: 6,
-        name: 'Hoodie Logo Line Night Brown',
-        href: '#',
-        imageSrc: '/imgPodutos/moletom2.png',
-        imageAlt: "Front of Hoodie Logo Line Night Brown.",
-        price: 'R$439', 
-    },
-    {
-        id: 7,
-        name: 'Jeans Pants Planet Black',
-        href: '#',
-        imageSrc: '/imgPodutos/calca1.png',
-        imageAlt: "Front of Jeans Pants Planet Black.",
-        price: 'R$599',
-    },
-    {
-        id: 8,
-        name: 'Jeans Pants Planet Blue',
-        href: '#',
-        imageSrc: '/imgPodutos/calca2.png',
-        imageAlt: "Front of Jeans Pants Planet Blue.",
-        price: 'R$599',
-    },
-    {
-        id: 9,
-        name: 'Smokey Pants Grey',
-        href: '#',
-        imageSrc: '/imgPodutos/calca3.png',
-        imageAlt: "Front of Smokey Pants Grey.",
-        price: 'R$489', 
-    },
+  {
+    id: 1,
+    category: ["T-SHIRTS", "ALL"],
+    name: "Tee Paris Black",
+    href: "#",
+    imageSrc: "/imgPodutos/camisa1.png",
+    imageAlt: "Front of men's Tee Paris Black.",
+    price: "R$199",
+  },
+  {
+    id: 2,
+    category: ["T-SHIRTS", "ALL"],
+    name: "Tee Paris White",
+    href: "#",
+    imageSrc: "/imgPodutos/camisa2.png",
+    imageAlt: "Front of men's Tee Paris White.",
+    price: "R$199",
+  },
+  {
+    id: 3,
+    category: ["NEW", "ALL", "JACKETS"],
+    name: "TJ x HIGH Denim Jacket",
+    href: "#",
+    imageSrc: "/imgPodutos/Blusa1.png",
+    imageAlt: "Front of TJ x HIGH Denim Jacket.",
+    price: "R$699",
+  },
+  {
+    id: 4,
+    category: ["NEW", "ALL", "JACKETS"],
+    name: "TJ x HIGH Nylon Jacket",
+    href: "#",
+    imageSrc: "/imgPodutos/blusa2.png",
+    imageAlt: "Front of TJ x HIGH Nylon Jacket.",
+    price: "R$849",
+  },
+  {
+    id: 5,
+    category: ["ALL", "SWEATSHIRTS"],
+    name: "Hoodie Logo Line Night Black",
+    href: "#",
+    imageSrc: "/imgPodutos/moletom1.png",
+    imageAlt: "Front of Hoodie Logo Line Night Black.",
+    price: "R$439",
+  },
+  {
+    id: 6,
+    category: ["ALL", "SWEATSHIRTS"],
+    name: "Hoodie Logo Line Night Brown",
+    href: "#",
+    imageSrc: "/imgPodutos/moletom2.png",
+    imageAlt: "Front of Hoodie Logo Line Night Brown.",
+    price: "R$439",
+  },
+  {
+    id: 7,
+    category: ["NEW", "ALL", "PANTS"],
+    name: "Jeans Pants Planet Black",
+    href: "#",
+    imageSrc: "/imgPodutos/calca1.png",
+    imageAlt: "Front of Jeans Pants Planet Black.",
+    price: "R$599",
+  },
+  {
+    id: 8,
+    category: ["NEW", "ALL", "PANTS"],
+    name: "Jeans Pants Planet Blue",
+    href: "#",
+    imageSrc: "/imgPodutos/calca2.png",
+    imageAlt: "Front of Jeans Pants Planet Blue.",
+    price: "R$599",
+  },
+  {
+    id: 9,
+    category: ["ALL", "PANTS"],
+    name: "Smokey Pants Grey",
+    href: "#",
+    imageSrc: "/imgPodutos/calca3.png",
+    imageAlt: "Front of Smokey Pants Grey.",
+    price: "R$489",
+  },
 ];
 
 export default function Shop() {
-    return (
-        <section className="flex flex-col items-center justify-between p-4 mt-6">
-            {/* Navegação */}
-            <nav className="w-full">
-                <ul className="flex justify-between items-center space-x-8">
-                    <li>NEW</li>
-                    <li>ALL</li>
-                    <li>H-LINE</li>
-                    <li>JACKETS</li>
-                    <li>LONGSLEEVES</li>
-                    <li>PANTS</li>
-                    <li>SHIRTS</li>
-                    <li>SHORTS</li>
-                    <li>SWEATSHIRTS</li>
-                    <li>TEES</li>
-                    <li>ACCESSORIES</li>
-                    <li>BAGS</li>
-                    <li>HATS</li>
-                    <li>SALE</li>
-                </ul>
-            </nav>
+  const [selectedCategory, setSelectedCategory] = useState<string>("ALL");
 
-           {/* Produtos */}
-           <div className="bg-white mt-6">
-                <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:gap-x-8">
-                        {products.map((product) => (
-                            <Link key={product.id} href={`/${product.id}`}>
+  // Filtra os produtos com base na categoria selecionada
+  const filteredProducts = products.filter((product) =>
+    product.category.includes(selectedCategory)
+  );
 
-                                <div className="group relative">
-                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-96">
-                                        <img
-                                            alt={product.imageAlt}
-                                            src={product.imageSrc}
-                                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                        />
-                                        <div className="absolute inset-0 flex flex-col justify-end bg-slate-50 bg-opacity-20 transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                            <div className="p-4 text-center">
-                                                <h3 className="text-sm">{product.name}</h3>
-                                                <p className="text-sm font-medium">{product.price}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                        ))}
+  return (
+    <section className="flex flex-col items-center justify-between p-4 mt-6">
+      {/* Navegação */}
+      <nav className="w-full">
+        <ul className="flex justify-between items-center space-x-7">
+          {[
+            "NEW",
+            "ALL",
+            "H-LINE",
+            "JACKETS",
+            "LONGSLEEVES",
+            "PANTS",
+            "SHIRTS",
+            "SHORTS",
+            "SWEATSHIRTS",
+            "T-SHIRTS",
+            "ACCESSORIES",
+            "BAGS",
+            "HATS",
+            "SALE",
+          ].map((category) => (
+            <li
+              key={category}
+              className={`cursor-pointer ${
+                selectedCategory === category ? "font-bold" : ""
+              }`}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Produtos */}
+      <div className="bg-white mt-6">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:gap-x-8">
+                {filteredProducts.map((product) => (
+                <Link key={product.id} href={`/produto/${product.id}`}>
+                    <div className="group relative">
+                    {/* Container da imagem */}
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-96">
+                        <img
+                        alt={product.imageAlt}
+                        src={product.imageSrc}
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                        />
                     </div>
-                </div>
+
+                    {/* Informações sobre o produto */}
+                    <div className="absolute inset-0 flex flex-col justify-end bg-slate-50 bg-opacity-20 transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                        <div className="p-4 text-center">
+                        <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
+                        <p className="text-sm font-semibold text-gray-700">{product.price}</p>
+                        </div>
+                    </div>
+                    </div>
+                </Link>
+                ))}
             </div>
-        </section>
-    );
+        </div>
+      </div>
+    </section>
+  );
 }
